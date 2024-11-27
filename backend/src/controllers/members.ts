26c26771323aed37bedb0a15cee12d096e974815
quythanh <quythanh.dev@gmail.com>
2024-11-27 13:59:12 +0700

    refactor: move mock data to seperate files

--- backend/src/controllers/members.ts
+++ backend/src/controllers/members.ts
@@ -1,11 +1,6 @@
 import type IMember from "../models/Member";
 import Controller from "./base";
-
-const MEMBERS: IMember[] = [
-	{ id: 1, name: "Phan Chí Hiếu" },
-	{ id: 2, name: "Tsàn Quý Thành" },
-	{ id: 3, name: "Phạm Đỗ Minh Vương" },
-];
+import MEMBERS from "../data/members";
 
 export default class MemberController extends Controller<IMember> {
 	constructor() {
---
+++ backend/src/data/members.ts
@@ -0,0 +1,9 @@
+import IMember from "../models/Member";
+
+const MEMBERS: IMember[] = [
+	{ id: 1, name: "Phan Chí Hiếu" },
+	{ id: 2, name: "Tsàn Quý Thành" },
+	{ id: 3, name: "Phạm Đỗ Minh Vương" },
+];
+
+export default MEMBERS;
